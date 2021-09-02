f86651094efee406c10c3601b62d2fb6f3be14b5
Frederic Mallet <Frederic.Mallet@univ-cotedazur.fr>
2021-09-02 13:28:31 +0200

    sync source from CM

--- sync.sh
+++ sync.sh
@@ -2,3 +2,4 @@
 cp ~/git/L3POO/2021-2022/jar/*.jar HELPERS/jar
 rsync -av ~/git/L3POO/2020-2021/share/ HELPERS/src/
 rsync -av ~/git/L3POO/2021-2022/TP/TP1.tests TPs/tests/
+rsync -av ~/git/L3POO/2021-2022/CM/CM1 CM
