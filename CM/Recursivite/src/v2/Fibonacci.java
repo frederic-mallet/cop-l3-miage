39614943e6a14acbcfa02abc3b27fa58dd0b3b61
Frederic Mallet <Frederic.Mallet@univ-cotedazur.fr>
2023-10-31 18:02:30 +0100

    Keep values from one call to the other

--- CM/Recursivite/src/v2/Fibonacci.java
+++ CM/Recursivite/src/v2/Fibonacci.java
@@ -4,23 +4,30 @@ import java.util.Arrays;
 
 import common.Time;
 
+/**
+ * Version plus compliquée que la version présentée en cours
+ * mais garde les valeurs d'un appel à l'autre.
+ */
 public class Fibonacci {
-	static int[] tab;
-	static int fibTab(int n) {
-		tab = new int[n + 1];
-		Arrays.fill(tab, -1);
-		tab[0] = 0;
-		tab[1] = 1;
-		return fibTab2(n);
+	// Init with Fibonacci(0) and Fibonacci(1)
+	// Not null, to make sure Arrays.copyOf works all the time
+	static int[] tab = {0, 1}; 
+	
+	static int fibonacci(int n) {
+		if (n >= tab.length)
+			tab = Arrays.copyOf(tab, n+1); // keep the values already computed, and fill with 0
+		
+		return fibTab(n);
 	}
-	private static int fibTab2(int n) {
-		if (tab[n] >= 0) return tab[n];
-		return tab[n] = fibTab2(n-1) + fibTab2(n-2);
+	private static int fibTab(int n) {
+		if (n == 0) return 0;
+		if (tab[n] > 0) return tab[n];
+		return tab[n] = fibTab(n-1) + fibTab(n-2);
 	}
 	public static void main(String[] args) {
 		for(int i = 5; i <= 50; i += 5) {
 			long avant = System.nanoTime();
-			fibTab(i);
+			fibonacci(i);
 			long apres = System.nanoTime();
 			System.out.println(Time.nanoSecondsToTime(apres - avant));
 		}
